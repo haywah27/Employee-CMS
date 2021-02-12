cb078542406b22e4a4db7f112f8e6575ab633ce9
haywah27 <hayleywahlroos15@gmail.com>
2021-02-11 18:12:10 -0800

    working on null bug on manager selection in new employee function

--- server.js
+++ server.js
@@ -23,27 +23,6 @@ const initialPrompt = [
     }
 ];
 
-const departData = [
-    {
-        type: "input",
-        message: "What department would you like to add?",
-        name: "newDepartment"
-    }
-];
-
-const roleData = [
-    {
-        type: "input",
-        message: "What role would you like to add?",
-        name: "newRole"
-    },
-    {
-        type: "input",
-        message: "What salary does this role earn?",
-        name: "newSalary"
-    }
-];
-
 
 let roleArr = [];
 function readRoles() {
@@ -58,30 +37,12 @@ function readRoles() {
         for (let i = 0; i < roles.length; i++){
             roleArr.push(roles[i]);
         }
-
+        // console.log("role.arr", roleArr)
         return roleArr;
 
     });
 };
 
-let deptArr = [];
-function readDepts() {
-    console.log('Selecting department names...\n');
-    connection.query('SELECT department FROM departments', (err, res) => {
-        if (err) throw err;
-
-        const depts = res.map(function(item){
-            return item['department']
-        });
-
-        for (let i = 0; i < depts.length; i++){
-            deptArr.push(depts[i]);
-        }
-
-        return roleArr;
-
-    });
-};
 
 
 function init(){
@@ -123,7 +84,7 @@ function init(){
 
 const viewEmployees = () => {
   console.log('Selecting all employees...\n');
-  connection.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.department, roles.salary, managers.manager_first_name FROM employees INNER JOIN roles ON roles.id = employees.role_id INNER JOIN departments ON departments.id = roles.department_id LEFT JOIN managers ON managers.id = employees.manager_id', (err, res) => {
+  connection.query('SELECT employees.id AS ID, CONCAT(employees.first_name," ", employees.last_name) AS Employee, roles.title AS Title, departments.department AS Department, roles.salary AS Salary, CONCAT(managers.manager_first_name," ", managers.manager_last_name) AS Manager FROM employees INNER JOIN roles ON roles.id = employees.role_id INNER JOIN departments ON departments.id = roles.department_id LEFT JOIN managers ON managers.id = employees.manager_id', (err, res) => {
     if (err) throw err;
     // Log all results of the SELECT statement
     console.log("///////////////////////////////////////////////////////////////////////////////////////");
@@ -187,7 +148,7 @@ function addEmployee(){
       {
         type: "list",
         message: "Who is their manager?",
-        choices: ["None", "Hayley", "Billie", "Aspen",],
+        choices: ["None", "Hayley Wahlroos", "Billie Thudds", "Aspen Threads",],
         name: "manager"
       }]).then(
         response => {
@@ -197,6 +158,7 @@ function addEmployee(){
             const lastName = response.lastName;
             let roleID = response.role;
             let managerID = response.manager;
+            console.log(typeof managerID)
             converRoleToNum(roleID, depIDArr);
 
             function converRoleToNum(roleID, depIDArr){
@@ -207,21 +169,24 @@ function addEmployee(){
                     depIDArr = roleValueToDeptID[0].department_id;
             
                     console.log("he", depIDArr)
+
+                    if (managerID === "Hayley Wahlroos"){
+                        managerID = 1;
+                    } else if (managerID === "Billie Frames"){
+                        managerID = 2;
+                    } else if (managerID === "Aspen Threads"){
+                        managerID = 3;
+                    } else {
+                        managerID = "NULL";
+                    }
+                    
                     createEmployee(firstName, lastName, depIDArr, managerID);
               });
             }
             
-            if (managerID === "Hayley"){
-                managerID = 1;
-            } else if (managerID === "Billie"){
-                managerID = 2;
-            } else if (managerID === "Aspen"){
-                managerID = 3;
-            } else {
-                managerID = "NULL";
-            }
-
             
+
+            console.log("man ID", managerID)
             init();
         });
 }
@@ -229,24 +194,24 @@ function addEmployee(){
 
 const createEmployee = (firstName, lastName, depIDArr, managerID) => {
     console.log('Inserting a new employee...\n');
-    if (managerID === "NULL"){
+    // if (managerID === "NULL"){
 
-        connection.query('INSERT INTO employees SET ?',
-        {
-          first_name: `${firstName}`,
-          last_name: `${lastName}`,
-          role_id: `${depIDArr}`,
-        },
-        (err, res) => {
-          if (err) throw err;
-          console.log(`${res.affectedRows} employee inserted!\n`);
-          // Call updateProduct AFTER the INSERT completes
-        //   updateEmployee(firstName, lastName, depIDArr, managerID);
-          init();
-        }
-      );
+    //     connection.query('INSERT INTO employees SET ?',
+    //     {
+    //       first_name: `${firstName}`,
+    //       last_name: `${lastName}`,
+    //       role_id: `${depIDArr}`,
+    //     },
+    //     (err, res) => {
+    //       if (err) throw err;
+    //       console.log(`${res.affectedRows} employee inserted!\n`);
+    //       // Call updateProduct AFTER the INSERT completes
+    //     //   updateEmployee(firstName, lastName, depIDArr, managerID);
+    //       init();
+    //     }
+    //   );
   
-    } else {
+    // } else {
     
     connection.query('INSERT INTO employees SET ?',
       {
@@ -262,7 +227,7 @@ const createEmployee = (firstName, lastName, depIDArr, managerID) => {
         init();
       }
     );
-    }
+    // }
     // logs the actual query being run
     // console.log(query.sql);
   };
@@ -302,10 +267,16 @@ const createEmployee = (firstName, lastName, depIDArr, managerID) => {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function addDepartment(){
-    inquirer.prompt(departData).then(
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What department would you like to add?",
+            name: "newDepartment"
+        }
+    ]).then(
         response => {
             console.log(response);
-            const dept = JSON.stringify(response.newDepartment);
+            const dept = response.newDepartment;
 
             createDept(dept);
             // console.log(response.newDepartment);
@@ -331,6 +302,23 @@ function createDept(dept) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let deptArr = [];
+function readDepts() {
+    console.log('Selecting department names...\n');
+    connection.query('SELECT department FROM departments', (err, res) => {
+        if (err) throw err;
+
+        const depts = res.map(function(item){
+            return item['department']
+        });
+
+        for (let i = 0; i < depts.length; i++){
+            deptArr.push(depts[i]);
+        }
+    });
+};
+
 
 function addRole(){
     readDepts();
@@ -398,7 +386,132 @@ function createRole(roleTitle, roleSalary, depIDArr) {
       }
     );
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+
+// function readEmployees(){
+//     console.log('Selecting employee names...\n');
+//     connection.query('SELECT first_name FROM employees', (err, res) => {
+//         if (err) throw err;
+
+//         const empFirst = res.map(function(item){
+//             return item['first_name']
+//         });
+
+//         for (let i = 0; i < empFirst.length; i++){
+//             empArr.push(empFirst[i]);
+//         }
+
+//         return empArr;
+
+//     });
+// };
+
+let employeeArr = [];
+function readEmployees(){
+        
+    console.log('Selecting employee names...\n');
+    connection.query('SELECT CONCAT(first_name," ", last_name) AS Manager FROM employees', (err, res) => {
+        if (err) throw err;
+
+        const empFirst = res.map(function(item){
+            return item['Manager']
+        });
+        
+        for (let i = 0; i < empFirst.length; i++){
+            // test.push(empFirst[i]);
+            employeeArr.push(empFirst[i])
+            
+        }
+        console.log("employee.arr", employeeArr)
+
+        readRoles();
+        inquirer.prompt(
+            [
+                {
+                    type: "list",
+                    message: "Which employee would you like to edit?",
+                    choices: employeeArr,
+                    name: "editName",
+                },
+                {
+                    type: "list",
+                    message: "Which role are they moving to?",
+                    choices: roleArr,
+                    name: "editRole",
+                }
+                
+            ])
+    });
+};
+
+function updateEmployee(){
+   
+    readEmployees();
+    readRoles();
+    inquirer.prompt(
+    [
+        {
+            type: "list",
+            message: "Which employee would you like to edit?",
+            choices: employeeArr,
+            name: "editName",
+        },
+        {
+            type: "list",
+            message: "Which role are they moving to?",
+            choices: roleArr,
+            name: "editRole",
+        }
+        
+    ])
+.then(
+        response => {
+
+            console.log(response);
+            const editName1 = response.editName;
+            let roleID = response.editRole;
+            findDeptID(roleID, depIDArr);
+
+            function findDeptID(roleID, depIDArr){
+                connection.query(("SELECT * FROM roles WHERE title = " + "'" + roleID + "'"), (err, res) => {
+                    if (err) throw err;
+                    let roleValueToDeptID = JSON.parse(JSON.stringify(res));
+            
+                    depIDArr = roleValueToDeptID[0].id;
+            
+                    console.log("dep ID", depIDArr)
+                    editRole(editName1, depIDArr);
+              });
+            }
+            
+            init();
+        });
+ 
+}
+
+
+function editRole(editName1, depIDArr){
+    console.log('Updating employee...\n');
+    let split = editName1.split(" ");
+    firstNameSplit = split[0];
+    lastNameSplit = split[1];
+    console.log("first name", firstNameSplit)
+    console.log("last name", lastNameSplit)
+    connection.query(
+      'UPDATE employees SET ? WHERE first_name = ' + "'" + firstNameSplit + "'" + "AND last_name = " + "'" + lastNameSplit + "'",
+      [
+        {
+            role_id: `${depIDArr}`,
+        },
+      ],
+      (err, res) => {
+        if (err) throw err;
+        console.log(`${res.affectedRows} employee updated!\n`);
+      }
+    ); 
+}
 
 // const updateDept = (dept)  => {
 //     console.log('Updating new department data...\n');
